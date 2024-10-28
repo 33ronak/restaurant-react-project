766d87f613629ce81ab39c991afa3fb9772b7796
33ronak <08arnab98@gmail.com>
2024-10-29 00:41:14 +0530

    Cart component functinality added and code refactored

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,18 +34,21 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
-    />));
+    />
+  ));
 
   return (
-    <div className={classes.meals}>
+    <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
       </Card>
-    </div>
+    </section>
   );
 };
+
 
 export default AvailableMeals;
