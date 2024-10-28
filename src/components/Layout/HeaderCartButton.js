7f4def96a75ba0f2f9f4756c809e775566178f0f
33ronak <08arnab98@gmail.com>
2024-10-28 22:50:34 +0530

    Cart component code refactored(Close buttonn functionality added)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
